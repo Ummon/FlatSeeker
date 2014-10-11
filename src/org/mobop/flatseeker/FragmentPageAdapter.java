bdb7af657319ea59fdeb0f0755c48cf865860a68
ice-blaze <franketip@gmail.com>
2014-10-11 15:22:35 +0200

    Add the UI base, tabs and swipes

---
+++ src/org/mobop/flatseeker/FragmentPageAdapter.java
@@ -0,0 +1,36 @@
+package org.mobop.flatseeker;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class FragmentPageAdapter extends FragmentPagerAdapter {
+
+    public FragmentPageAdapter(FragmentManager fm) {
+	super(fm);
+	// TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Fragment getItem(int arg0) {
+
+	switch (arg0) {
+	case 0:
+	    return new EditFragment();
+	case 1:
+	    return new ListFragment();
+	case 2:
+	    return new MapFragment();
+	default:
+	    break;
+	}
+	return null;
+    }
+
+    @Override
+    public int getCount() {
+	// we got 3 fragments list/map/edit
+	return 3;
+    }
+
+}
--- src/org/mobop/flatseeker/Main.java
+++ src/org/mobop/flatseeker/Main.java
@@ -1,34 +1,66 @@
 package org.mobop.flatseeker;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.app.Notification.Action;
+import android.net.wifi.p2p.WifiP2pManager.ActionListener;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class Main extends Activity {
+public class Main extends FragmentActivity implements ActionBar.TabListener {
+    ActionBar actionBar;
+    ViewPager viewPager;
+    FragmentPageAdapter ft;
 
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.main);
-   }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.main);
 
-   @Override
-   public boolean onCreateOptionsMenu(Menu menu) {
-      // Inflate the menu; this adds items to the action bar if it is present.
-      getMenuInflater().inflate(R.menu.main, menu);
-      return true;
-   }
+	viewPager = (ViewPager) findViewById(R.id.pager);
+	ft = new FragmentPageAdapter(getSupportFragmentManager());
 
-   @Override
-   public boolean onOptionsItemSelected(MenuItem item) {
-      // Handle action bar item clicks here. The action bar will
-      // automatically handle clicks on the Home/Up button, so long
-      // as you specify a parent activity in AndroidManifest.xml.
-      int id = item.getItemId();
-      if (id == R.id.action_settings) {
-         return true;
-      }
-      return super.onOptionsItemSelected(item);
-   }
+	actionBar = getActionBar();
+	viewPager.setAdapter(ft);
+	actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+	actionBar.addTab(actionBar.newTab().setText("Edit").setTabListener(this));
+	actionBar.addTab(actionBar.newTab().setText("List").setTabListener(this));
+	actionBar.addTab(actionBar.newTab().setText("Map").setTabListener(this));
+
+	viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+	    @Override
+	    public void onPageSelected(int arg0) {
+		actionBar.setSelectedNavigationItem(arg0);
+	    }
+
+	    @Override
+	    public void onPageScrolled(int arg0, float arg1, int arg2) {
+	    }
+
+	    @Override
+	    public void onPageScrollStateChanged(int arg0) {
+	    }
+	});
+    }
+
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+	viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+    }
 }
